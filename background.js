b2a9969874e421c063148ab723ed46317e7d715f
Alan <zeyu.weng@gmail.com>
2023-11-11 14:21:09 -0500

    able to fetch tab name and url

--- background.js
+++ background.js
@@ -1,1 +1,19 @@
 // This file will act as the middleware that helps facilitate resources between content-script & popup
+
+const handleTabActivation = async () => {
+    const [tab] = await chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true,
+    });
+    const url = tab.url;
+    // const tabId = tab.id;
+    const tabName = tab.title
+
+    console.log(tabName)
+    console.log(url)
+  };
+
+  chrome.tabs.onUpdated.addListener(() => {
+    handleTabActivation();
+  });
+  
